26154e0e4bf92a0318502c916da1da46cafaa787
TristanV <tristan.vanrullen@gmail.com>
2022-10-07 11:40:19 +0200

    AIT-20 updating config for web app

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ import os
 class DefaultConfig:
     """Configuration for the bot."""
 
-    PORT = 3978
+    PORT = 8000 # 3978
     #note that the user managed identity has no password (blank) but the environment variable cannot be blank, so we use a single space that needs to be removed
     #we also want to test the bot before registering it on azure. in this case the MicrosoftAppId must remain blank too in the environment variables 
     APP_ID = os.environ.get("MicrosoftAppId", "").strip()  
-    #APP_ID = ""
+    # APP_ID = ""
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "").strip()
-    #APP_PASSWORD = ""
+    # APP_PASSWORD = ""
     LUIS_APP_ID = os.environ.get("LuisAppId", "")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
